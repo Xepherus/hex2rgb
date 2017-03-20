e95de5256d3611927bf1f3ef31e093db8b14f01d
Xepherus <lestep03@live.com>
2017-03-20 10:51:04 -0500

    Added comments

--- hex2rgb.py
+++ hex2rgb.py
@@ -1,5 +1,10 @@
-def rgb_hex():
+"""
+Simple script for converting HEX to RGB and RGB to Hex
+"""
+
+def rgb_hex(): # Function for RGB to HEX conversion
   invalid_msg = "Error, Invalid Input."
+  # Input collection
   red = int(raw_input("Enter red (R) value: "))
   if red < 0 or red > 255:
     print invalid_msg
@@ -12,25 +17,26 @@ def rgb_hex():
   if blue < 0 or blue > 255:
     print invalid_msg
     return
-  val = (red << 16) + (green << 8) + blue
-  print "%s" % (hex(val)[2:]).upper()
-  
-def hex_rgb():
-  hex_val = raw_input("Enter a hex value: ")
-  if len(hex_val) != 6:
+  val = (red << 16) + (green << 8) + blue # Shifts bits left by pre-determined amount
+  print "%s" % (hex(val)[2:]).upper() # Final output
+
+def hex_rgb(): # Function for HEX to RGB conversion
+  hex_val = raw_input("Enter a hex value: ") # Request input
+  if len(hex_val) != 6: # Tests for valid Hexidecimal input
     print "Error, Invalid Input"
     return
   else:
-    hex_val = int(hex_val, 16)
+    hex_val = int(hex_val, 16) # Converts to base 16 int
+  # Bit shifting and conversion
   two_hex_digits = 2 ** 8
   blue = hex_val % two_hex_digits
   hex_val = hex_val >> 8
   green = hex_val % two_hex_digits
   hex_val = hex_val >> 8
   red = hex_val % two_hex_digits
-  print "Red: %s Green: %s Blue: %s" % (red, green, blue)
-  
-def convert():
+  print "Red: %s Green: %s Blue: %s" % (red, green, blue) # Final output
+
+def convert(): # Menu function
   while True:
     option = raw_input("Enter 1 to convert RGB to HEX. Enter 2 to convert HEX to RGB. Enter X to Exit: ")
     if option == '1':
